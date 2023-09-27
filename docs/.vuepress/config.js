65d2432f197f5111f50ab121d5d09ea95b461a81
zhousujuan <179997445@qq.com>
2023-09-27 15:19:28 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,22 @@
 module.exports = {
-  title: "知时",
+  title: "zhousujuan",
   description: "我的个人空间",
+  base: "/page/",
+
+  locales: {
+    //设置语言
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+  themeConfig: {
+    sidebar: [
+      {
+        title: "第一章",
+        path: '/',
+        collapsable: false, // 不折叠
+        children: [{ title: "第一节", path: "/" }],
+      },
+    ],
+  },
 };
