0a6430632001ee929dcd8ac8061dd5999f3427af
zhousujuan <179997445@qq.com>
2023-09-28 11:18:52 +0800

    fix

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: "zhousujuan",
+  description: "我的个人空间",
+  base: "/studyNotes/",
+  locales: {
+    //设置语言
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "GitHub", link: "https://github.com/zhousujuan" },
+    ],
+    sidebar: [
+      {
+        title: "第一章",
+        path: "/",
+        collapsable: false, // 不折叠
+        children: [
+          { title: "第一节", path: "/" },
+          { title: "第二节", path: "/" },
+        ],
+      },
+    ],
+  },
+};
